bb97ff35da158a81f8f1f868ba81f87cba1f3d82
aoliveira_italtel <aoliveira_italtel@timbrasil.com.br>
2026-07-30 15:07:00 -0300

    removendo arquivos de lixo

--- coverage-hub/agent/teste_geral.py
+++
@@ -1,7 +0,0 @@
-try:
-    from .excel_municipios import calcular_score_oportunidade_5g
-except ImportError:
-    from excel_municipios import calcular_score_oportunidade_5g
-
-
-print(calcular_score_oportunidade_5g(uf="PR", limite=20))
